bfd2f63c81cb8b260d8f478aaf4c9ee9effdf1f3
rokas_simon <rokas.rokanas@gmail.com>
2022-11-23 01:01:04 +0200

    Initial

---
+++ Source/TableTennis/EmptyClassForProject.cpp
@@ -0,0 +1,12 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+//#include "TableTennis.h"
+#include "EmptyClassForProject.h"
+
+EmptyClassForProject::EmptyClassForProject()
+{
+}
+
+EmptyClassForProject::~EmptyClassForProject()
+{
+}
